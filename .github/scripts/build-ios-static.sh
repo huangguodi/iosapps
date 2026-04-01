7b4666c53d007ceddcf1c63bf4d571316c0890cd
admin <admin@local>
2026-04-01 09:46:24 +0800

    fix(ios): improve generated gomobile headers

--- .github/scripts/build-ios-static.sh
+++ .github/scripts/build-ios-static.sh
@@ -59,10 +59,17 @@ done < <(find "$HEADER_DIR" -maxdepth 1 -type f \( -name "*.h" -o -name "*.modul
 
 if grep -q '#include "ref.h"' "$OUT_DIR/libmihomo.h" && [[ ! -f "$OUT_DIR/ref.h" ]]; then
   cat > "$OUT_DIR/ref.h" <<'EOF'
-#ifndef GO_SEQ_REF_H
-#define GO_SEQ_REF_H
-@import Foundation;
-@protocol goSeqRefInterface <NSObject>
+#ifndef __GO_REF_HDR__
+#define __GO_REF_HDR__
+#include <Foundation/Foundation.h>
+@interface GoSeqRef : NSObject {}
+@property(readonly) int32_t refnum;
+@property(strong) id obj;
+- (instancetype)initWithRefnum:(int32_t)refnum obj:(id)obj;
+- (int32_t)incNum;
+@end
+@protocol goSeqRefInterface
+- (GoSeqRef*) _ref;
 @end
 #endif
 EOF
@@ -70,9 +77,21 @@ fi
 
 if grep -q '#include "Universe.objc.h"' "$OUT_DIR/libmihomo.h" && [[ ! -f "$OUT_DIR/Universe.objc.h" ]]; then
   cat > "$OUT_DIR/Universe.objc.h" <<'EOF'
-#ifndef GO_UNIVERSE_OBJC_H
-#define GO_UNIVERSE_OBJC_H
+#ifndef __Universe_H__
+#define __Universe_H__
 @import Foundation;
+#include "ref.h"
+@protocol Universeerror;
+@class Universeerror;
+@protocol Universeerror <NSObject>
+- (NSString* _Nonnull)error;
+@end
+@interface Universeerror : NSError <goSeqRefInterface, Universeerror> {
+}
+@property(strong, readonly) _Nonnull GoSeqRef* _ref;
+- (nonnull instancetype)initWithRef:(_Nonnull GoSeqRef*)ref;
+- (NSString* _Nonnull)error;
+@end
 #endif
 EOF
 fi
